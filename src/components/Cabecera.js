402a8299d9f2cd0556cb3566d27e5e2e873bd485
Jennifer Colman <jenniferecolmanb@hotmail.com>
2022-08-29 22:29:56 -0300

    entregable frontend 3 colman jennifer

---
+++ src/components/Cabecera.js
@@ -0,0 +1,20 @@
+// El componente Cabera no tiene componentes hijos.
+// ESTADO: Cabecera no tiene estado.
+// MÉTODOS: Cabecera no tiene métodos.
+// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente
+// Maqueta de Cabecera:
+//    h1
+//    p > span     (el span mostrará la cantidad recibida por props)
+
+import React from 'react'
+
+export default function Cabecera({productosComprados}) {
+  
+  return (
+    <header>
+        {/* maquetar Cabecera aquí */}
+        <h1>Carrito de Compras</h1>
+        <p>Cantidad de productos: <span>{productosComprados}</span></p>  
+    </header>
+  );
+};
